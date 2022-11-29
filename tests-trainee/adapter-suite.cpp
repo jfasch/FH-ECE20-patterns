8c568321f5ff22081e95af39f6e2913478316e31
Joerg Faschingbauer <jf@faschingbauer.co.at>
2022-11-29 09:19:39 +0100

    implement w1sensor

--- tests-trainee/adapter-suite.cpp
+++ tests-trainee/adapter-suite.cpp
@@ -7,12 +7,12 @@
 
 TEST(adapter_suite, basic)
 {
-    // buy most sophisticated sensor from A, and see if it works
-    AConstantSensor sensor_from_a(36.5);
-    ASSERT_FLOAT_EQ(sensor_from_a.get_temperature_fahrenheit(), 36.5);
+    // // buy most sophisticated sensor from A, and see if it works
+    // AConstantSensor sensor_from_a(36.5);
+    // ASSERT_FLOAT_EQ(sensor_from_a.get_temperature_fahrenheit(), 36.5);
 
-    // adapt it into our hierarchy, converting its Fahrenheit
-    // measurement into Celsius
-    ASensor_Adapter a_adapter(&sensor_from_a);
-    ASSERT_FLOAT_EQ(a_adapter.get_temperature(), (36.5 - 32) * 5/9);
+    // // adapt it into our hierarchy, converting its Fahrenheit
+    // // measurement into Celsius
+    // ASensor_Adapter a_adapter(&sensor_from_a);
+    // ASSERT_FLOAT_EQ(a_adapter.get_temperature(), (36.5 - 32) * 5/9);
 }
